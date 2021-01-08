61f9508bda5c34f9f1b8e0f6a109f25d3d4f8790
mattedamo <mattedamo@gmail.com>
2021-01-08 11:25:47 +0100

    aa

--- set-ns.py
+++ set-ns.py
@@ -4,7 +4,6 @@ import yaml, os
 def main():
     with open("./file.yaml") as file:
         input = yaml.load(file, Loader=yaml.FullLoader)
-    
     if "ns" in input.keys():
         ns = { "ns" : os.getenv("NS") }
         input.update(ns)
@@ -12,7 +11,7 @@ def main():
         input["ns"] = os.getenv("NS")
     print(os.getenv("NS"))
     print(input)
-    with open("./file", "w") as file:
+    with open("./file.yaml", "w") as file:
                 yaml.dump(input, file)
 
 if __name__ == '__main__':
